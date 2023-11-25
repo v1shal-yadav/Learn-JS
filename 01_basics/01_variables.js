4c1b95f87b423133c7ccb7de56a4d96d88fe40aa
Vishal Yadav <83578850+v1shal-yadav@users.noreply.github.com>
2023-11-25 10:09:42 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 1243
+let accountEmail = "vishal@gmail.com"
+var accountPassword = "weer@#23"
+accountCity = "varanasi" // this is not a standard practise
+let accountState 
+
+// accountId = 2 // this is not allowed 
+
+accountEmail = "yadav@gmail.com"
+accountPassword = "r234f2@@@"
+accountCity = "Delhi"
+
+console.log("accountPassword")
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
+
+/*
+let and const are used now a days.
+Prefer not to use var beacause of issue in block scope and functional scope.
+A variable is undefined if it is declared but not assigned any value.
+You may or may not use semi-colon in javascript.
+Javascript engine is hidden inside browser.
+code readability should be high.
+Documentation -> tc39(official by ECMA) and mdn (by mozilla)
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using node.js, not browser
+
+let name = "vishal"
+let age = 22
+let isLoggedIn = true; 
+
+//      *** primitve datatypes *** 
+// number => 2 to the power 53
+// bigInt 
+// string => ""
+// boolean = true/false 
+// null => standalone value (representation of empty value)
+// undefined => value is not assigned. 
+// symbol => unique 
+
+// object 
+
+console.log(typeof null) // => object
+console.log(typeof undefined) // => undefined
+
+
+
