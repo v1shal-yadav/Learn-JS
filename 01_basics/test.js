aa51987dee0f90e3eff5da9b95ede9c470735029
Vishal Yadav <83578850+v1shal-yadav@users.noreply.github.com>
2023-11-26 17:47:51 +0000

    to setup the codespace for javascript in gitHub

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,9 @@
 console.log("vishal")
+/**
+to setup the codespace for javascript in gitHub 
+    1. open command palette file -> view -> Commmand palette.
+    2. search for container 
+    3. search for node.js and javascript. 
+    4. rebuild now.
+    5. codespace ready. 
+ */
