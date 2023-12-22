39d6645437da019221c0fa3e315a6ccba3e083fc
Vishal Yadav <83578850+v1shal-yadav@users.noreply.github.com>
2023-12-22 12:09:06 +0000

    learn objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,15 +1,48 @@
 // *** objects *** 
 
+const mySym = Symbol("key1")
+
 const JsUser = {
     name: "vishal", // => name is treated as a string "name"
+    "full name": "vishal yadav",
     age: 22,
     location: "varanasi", 
-    email: "vishal@email.com"
+    email: "vishal@email.com",
     isLoggedIn: false,
-    lastLogInDays: ["monday", "tuesday"]
+    lastLogInDays: ["monday", "tuesday"],
+    [mySym]: "myKey1"  // => syntax for using symbol in objects
+};
+console.log(JsUser.email) // => vishal@email.com
+console.log(JsUser["email"]) // => vishal@email.com
+console.log(JsUser["full name"]) // => vishal yadav
+console.log(JsUser[mySym]) // => myKey1
+
+JsUser.email = "vishal@google.com"
+console.log(JsUser.email); // => vishal@google.com
+// Object.freeze(JsUser)
+JsUser.email = "vishal@chatGPT.com"
+console.log(JsUser.email); // => vishal@google.com
+
+console.log(JsUser);
+/* 
+{
+  name: 'vishal',
+  'full name': 'vishal yadav',
+  age: 22,
+  location: 'varanasi',
+  email: 'vishal@google.com',
+  isLoggedIn: false,
+  lastLogInDays: [ 'monday', 'tuesday' ],
+  [Symbol(key1)]: 'myKey1'
 }
-console.log(JsUser.email)
-console.log(JsUser[email])
+*/
+
+JsUser.greetings = function() {
+    console.log("Hello, JS user");
+}
+console.log(JsUser.greetings); // => [Function (anonymous)]
+console.log(JsUser.greetings()); // => [Function (anonymous)]
+
 
 
 
