f5b9e0931feb8be4c602dcdf79e02bffa30a7c70
Vishal Yadav <yaimvishal@gmail.com>
2023-12-30 09:15:37 +0530

    control flow

---
+++ one.js
@@ -0,0 +1,16 @@
+// if 
+const isUserLoggedIn = true
+
+// if(2 == "2") { // (2 == "2") is a true codition
+//     console.log("executed") 
+// }
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+// if(true) {
+//     console.log(1);
+// } else if(true) {
+//     console.log(2);
+// }
+
+
---
+++ switch.js
@@ -0,0 +1,18 @@
+// *** switch *** 
+
+const month = 3; 
+
+switch(month) {
+    case 1: // or case "jan": 
+        console.log("January");
+        break;
+    case 2: 
+        console.log("february");
+        break;
+    case 3: 
+        console.log("March");
+        break;
+    default: 
+        console.log("default case matched");
+        break;
+}
