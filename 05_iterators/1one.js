198a3f40944e8176bc81061184740c957114ce0c
Vishal Yadav <yaimvishal@gmail.com>
2024-01-01 09:36:03 +0530

    loop

---
+++ 05_iterators/1one.js
@@ -0,0 +1,29 @@
+// for
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+let myArray = ["flash", "batman", "superman"]
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+}
+
+// break and continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if(index == 5){
+//     console.log(`Detected 5`);
+//     break
+//     }
+//     console.log(`value of i is ${index}`)
+// }
+// for (let index = 1; index <= 20; index++) {
+//     if(index == 5){
+//     console.log(`Detected 5`);
+//     continue
+//     }
+//     console.log(`value of i is ${index}`)
+// }
+
---
+++ 05_iterators/two.js
@@ -0,0 +1,19 @@
+// while
+
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`)
+    index = index + 2
+}
+
+// do-while loop (used rarely) 
+
+let score = 1
+
+do{
+    console.log(`score is ${score}`)
+    score++
+}
+while(score <= 10)
+
+// 
