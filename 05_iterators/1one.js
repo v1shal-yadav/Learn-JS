abe7f36f5253d4febf8aebbecfa87540a9e250f6
Vishal Yadav <yaimvishal@gmail.com>
2024-01-27 23:14:28 +0530

    functions

--- 05_iterators/1one.js
+++ 05_iterators/1one.js
@@ -7,7 +7,7 @@
 let myArray = ["flash", "batman", "superman"]
 for (let index = 0; index < myArray.length; index++) {
     const element = myArray[index];
-    console.log(element);
+    console.log(element); 
 }
 /* 
     *** break and continue ***
